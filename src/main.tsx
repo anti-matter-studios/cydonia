f3bf58b7c5190734b622cfccd664dbf40a0d80fd
Akane SHIMOKA <git_commit@jb-caillaud.fr>
2026-07-01 16:39:18 +0200

    wip: starts working on the overall disposition and background style of the renderer

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import Cydonia from "@/component/organism/cydonia";
-import "@/style/index.css";
 
 createRoot(document.getElementById("root") as HTMLElement)
     .render(<StrictMode><Cydonia /></StrictMode>);
